3c56d2f0edd52667d82d12606d6c17fad7c1f59d
eslamibrahim30 <eslamelbhnaswee110@gmail.com>
2026-06-17 12:42:24 +0300

    fix: uploading contract bug

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -45,7 +45,7 @@ app.use(requestIdMiddleware);
 app.use(helmet());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173", "http://localhost:8080"],
     credentials: true,
   }),
 );
